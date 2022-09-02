6e23f3114bc4ad08bf05f34f203f5cbdca3b0dbb
vitordiniz25 <vitordiniz25@gmail.com>
2022-09-02 16:49:39 -0300

    estilizando home page

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -19,12 +19,41 @@ export const FormContainer = styled.div`
   width: 100%;
   display: flex;
   align-items: center;
-  justify-content: cneter;
+  justify-content: center;
   gap: 0.5rem;
   color: ${(props) => props.theme['gray-100']};
   font-size: 1.125rem;
   font-weight: bold;
   flex-wrap: wrap;
+`
+
+const BaseInput = styled.input`
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme['gray-500']};
+  font-weight: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme['green-500']};
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
+`
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+  font-size: 1rem;
+`
+
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
 `
 
 export const CountdownContainer = styled.div`
@@ -45,10 +74,38 @@ export const CountdownContainer = styled.div`
 
 export const Separator = styled.div`
   padding: 2rem 0;
-  color: ${(props) => props.theme['gray-500']};
+  color: ${(props) => props.theme['green-500']};
 
   width: 4rem;
   overflow: hidden;
   display: flex;
   justify-content: center;
+`
+
+export const StartCountdownButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  gap: 0.5rem;
+  font-weight: bold;
+
+  cursor: pointer;
+
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-100']};
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['green-700']};
+  }
 `
